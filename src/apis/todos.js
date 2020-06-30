3427e8f084d5ba8313748f92a40425876f05c387
Vivian Zhang <taiwei426@gmail.com>
2020-06-30 13:29:56 -0600

    changed heroku url

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL : 'http://localhost:3001'
-    baseURL: 'https://vivianzhang-todo-app-api.herokuapp.com/todos'
+    baseURL: 'https://vivianzhang-todo-app-api.herokuapp.com'
 })
