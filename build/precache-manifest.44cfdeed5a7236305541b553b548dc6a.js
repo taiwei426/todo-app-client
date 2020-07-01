07470c50f580ffae290efa9c2f0cd6f0d4fc0060
Vivian Zhang <taiwei426@gmail.com>
2020-07-01 14:37:37 -0600

    new build

---
+++ build/precache-manifest.44cfdeed5a7236305541b553b548dc6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9304da23e7c0d878ee219e60c58f352b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "c285627e83a2fd944666",
+    "url": "/todo-app/static/js/2.2502eebc.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todo-app/static/js/2.2502eebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64e1ce9c900a7133bf8",
+    "url": "/todo-app/static/js/main.bf7049f9.chunk.js"
+  },
+  {
+    "revision": "feddca2058b1f499b2e3",
+    "url": "/todo-app/static/js/runtime-main.d6457ad5.js"
+  }
+]);
