b344f64497527d834d4da7f4b982f8f148122fe1
Vivian Zhang <taiwei426@gmail.com>
2020-06-30 22:57:49 -0600

    fixed random error that only happened in prod. wouldn't let me edit any of the items

---
+++ build/precache-manifest.7dc4a59b7abf3c137da1518fcf1d18d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83558ef09fe313c35ddc28de5596ba7",
+    "url": "/todo-app-client/index.html"
+  },
+  {
+    "revision": "c285627e83a2fd944666",
+    "url": "/todo-app-client/static/js/2.2502eebc.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todo-app-client/static/js/2.2502eebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff52ff3ffe03284a23a",
+    "url": "/todo-app-client/static/js/main.f7708236.chunk.js"
+  },
+  {
+    "revision": "1bdabc4dd2829e153dfa",
+    "url": "/todo-app-client/static/js/runtime-main.a770c2e3.js"
+  }
+]);
